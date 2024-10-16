b362c3b9daf4a07c9d08ba7ddd14152714b6ec67
Wonjin Kim <wjkim9653@gmail.com>
2024-10-16 09:47:39 +0000

    fix

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -18,7 +18,7 @@ pip install numpy==1.23.5
 pip install vllm==0.4.1
 pip install numpy==1.23.5
 pip install accelerate==0.31.0
-pip intall flax==0.8.4
+pip install flax==0.8.4
 pip install numpy==1.23.5
 pip install -U jax[cuda12]==0.4.28  -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install -U jaxlib==0.4.28+cuda12.cudnn89  -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
